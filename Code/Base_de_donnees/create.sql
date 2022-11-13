4dbffbeea592463efd19280c82fad3aa99f065db
Floris <Floris.robart11@gmail.com>
2022-11-13 22:43:33 +0100

    update bado

--- Code/Base_de_donnees/create.sql
+++ Code/Base_de_donnees/create.sql
@@ -6,10 +6,12 @@ DEFAULT COLLATE utf8_general_ci;
 
 
 
--- MODELE
-CREATE TABLE MODELE (
-    nomModele  VARCHAR(42) PRIMARY KEY,
-    nomMarque     VARCHAR(42) REFERENCES Marque(nommarque)
+-- PHOTO
+CREATE TABLE PHOTO (
+    idPhoto    SERIAL PRIMARY KEY,
+    nomPhoto   VARCHAR(42),
+    datePhoto  DATE,
+    idVoiture  INT REFERENCES VOITURE (idvoiture)
 );
 
 
@@ -21,25 +23,21 @@ CREATE TABLE VOITURE (
 );
 
 
--- PHOTO
-CREATE TABLE PHOTO (
-    idPhoto    SERIAL PRIMARY KEY,
-    nomPhoto   VARCHAR(42),
-    datePhoto  DATE,
-    idVoiture  INT REFERENCES VOITURE (idvoiture)
-);
-
-
--- COULEUR_VOITURE
-CREATE TABLE COULEUR_VOITURE (
-    idVoiture  INT         REFERENCES VOITURE (idvoiture ),
-    nomCouleur VARCHAR(42) REFERENCES COULEUR (nomcouleur),
-    PRIMARY KEY (idvoiture, nomcouleur)
-);
-
-
 -- MARQUE
 CREATE TABLE MARQUE (
-    nomMarque VARCHAR(42) PRIMARY KEY,
-    pays      VARCHAR(42)
+    nomMarque         VARCHAR(42) PRIMARY KEY,
+    paysOrigineMarque VARCHAR(42)
+);
+
+
+-- PREPARATEUR
+CREATE TABLE PREPARATEUR (
+    nomPreparateur   VARCHAR(42) PRIMARY KEY,
+    paysOriginePrepa VARCHAR(42)
+);
+
+
+-- MOTEUR
+CREATE TABLE MOTEUR (
+    idMoteur   SERIAL PRIMARY KEY,
 );
